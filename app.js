f07e4096d449578a3e41c30180034b5264e54abd
Daniel Cook <63421663+danielmcook@users.noreply.github.com>
2024-04-07 23:05:10 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -7,19 +7,18 @@ function sleep(ms) {
 
 $(document).ready(function(){
     let button = document.getElementById("explore");
-    let title = document.getElementById("title");   
-    let timeline = document.getElementById("timeline");
-    let all = document.getElementById("all");
+    let title = document.getElementById("hero");   
+    let main = document.getElementById("main");
     
-    timeline.style.display = "none";
-    all.style.display = "none";
+    main.style.display = "none";
+    
     button.addEventListener("click", function() {
         title.style.animation = "fadeOut .5s linear";
         title.style.opacity = "0";
-        timeline.style.display = "block";
-        all.style.display = "block";
-        timeline.style.animation = "fadeIn .5s linear";
-        all.style.animation = "fadeIn .5s linear";
+        title.style.display = "none";
+        main.style.display = "block";
+        main.style.animation = "fadeIn .5s linear";
+     
        // window.scrollTo(0,0);
     });
     
@@ -32,30 +31,38 @@ $(document).ready(function(){
     let scroll = window.scrollX;
 
     function checkScroll(x) {
-        if (x <= window.innerWidth) {
+        console.log(x);
+        if (x <= window.innerHeight) {
             one.style.animation = "grow .25s linear";
             one.style.fontSize = "2em";
             two.style.animation = "shrink .25 linear";
             two.style.fontSize = "1em";
-        } else if (x >= window.innerWidth && x <= window.innerWidth * 2) {
+        } else if (x >= window.innerHeight / 3 && x <= window.innerHeight * 2) {
             one.style.animation = "shrink .25 linear";
             one.style.fontSize = "1em";
             three.style.animation = "shrink .25 linear";
             three.style.fontSize = "1em";
             two.style.animation = "grow .25s linear";
             two.style.fontSize = "2em";
-        } else if (x >= window.innerWidth * 2 && x <= window.innerWidth * 3) {
+        } else if (x >= window.innerHeight * 2 && x <= window.innerHeight * 3) {
             two.style.animation = "shrink .25 linear";
             two.style.fontSize = "1em";
             three.style.animation = "grow .25s linear";
             three.style.fontSize = "2em";
+            four.style.animation = "shrink .25 linear";
+            four.style.fontSize = "1em";
+        } else if (x >= window.innerHeight * 3.5) {
+            three.style.animation = "shrink .25 linear";
+            three.style.fontSize = "1em";
+            four.style.animation = "grow .25s linear";
+            four.style.fontSize = "2em";
         }
     }
 
     checkScroll(0);
 
     window.addEventListener('scroll', function() {
-        scroll = window.scrollX;
+        scroll = window.scrollY;
         checkScroll(scroll);
     });
 });
